8d42dff54b15795d9ef6060e3cd118d258ebb7c4
Chris Eun <eunchristopher@gmail.com>
2022-11-27 19:08:35 -0800

    background update

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-function newBackground(url, height) {
-  let background = document.createElement('img')
-  background.src = url
-  background.style.position = 'fixed'
-  background.style.bottom = 0;
-  background.style.width = '100%'
-  background.style.height = height + 'vh'
-  document.body.append(background)
+// function newBackground(url, height) {
+//   let background = document.createElement('img')
+//   background.src = url
+//   background.style.position = 'fixed'
+//   background.style.bottom = 0;
+//   background.style.width = '100px'
+//   background.style.height = '100px'
+//   document.body.append(background)
+// }
 
+function tile(url, left, bottom, width, height){
+  for(let i = 0; i < height; i++){
+      for(let w =0; w < width; w++){
+      newImage(url, left + w*100, bottom+i*100)
+    }
+  }
 }
 
+let horizon = window.innerHeight / 1.75;
+let heightOfSky = window.innerHeight-horizon;
+let heightOfGrass = horizon;
+
+tile('assets/sky.png', 0, horizon, window.innerWidth, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth, heightOfGrass/100)
 
 
-newBackground('assets/sky.png', 100)
-newBackground('assets/grass.png', 70)
+
+// newBackground('assets/sky.png', 100)
+// newBackground('assets/grass.png', 70)
 
 
 
@@ -27,12 +41,15 @@ function newImage(url, left, bottom) {
 
 }
 
+
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+newImage('assets/green-character/east.gif', 140, 430)
 
 
 
@@ -48,7 +65,7 @@ function newItem(url, left, bottom) {
   document.body.append(object)
 
 
-  object.addEventListener('dblclick', function () {
+  object.addEventListener('dblclick', () => {
     object.remove()
   })
 
